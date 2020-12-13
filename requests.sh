b548c8331bc53f56f94048f1ebe614d559b98876
SanderVerbeken <43341289+SanderVerbeken@users.noreply.github.com>
2020-12-13 12:51:37 +0100

    Update requests.sh

--- requests.sh
+++ requests.sh
@@ -2,6 +2,6 @@
 
 for i in $(seq 1 20);
 do
-  echo "we do a curl of http://193.168.0.173:8080, and we get this as response:"
-  curl http://192.168.0.173:8080
+  echo "we do a curl of http://localhost:8080, and we get this as response:"
+  curl http://localhost:8080
 done
